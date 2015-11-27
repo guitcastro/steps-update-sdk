2f52d1ba02b2d1171d3ffbc175d885cdba2d3881
guitcastro <guilherme.castro@appprova.com.br>
2015-11-27 17:56:17 -0200

    Detect if a tool is already installed, and don't install it again #1

--- step.sh
+++ step.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-set -e
+#tools=
+#build_tools=23.0.1,22.1.1
+#platform=23,22,20,21
+#system_images='sys-img-x86-android-23,sys-img-armeabi-v7a-android-tv-23,sys-img-armeabi-v7a-android-22,sys-img-x86-android-tv-23,sys-img-armeabi-v7a-android-wear-23,sys-img-x86-android-wear-23'
 
-tools=	
-platform_tools=both
-platform=23,22,20
+function checkIfIsInstalled {
+	local array="$(ls $ANDROID_HOME/$1)"
+	local i
+	for i in ${array[@]}
+	do				
+	    if [ "$i" == "$2" ] ; then
+	        return 0
+	    fi
+	done
+	return 1
+}
 
 function appendFilter {
 	if [ -z "$FILTER" ]; then
@@ -12,6 +23,28 @@ function appendFilter {
 	else
 		FILTER="$FILTER,$1"
 	fi			
+}
+
+function checkIfIsSystemImageInstalled {
+	local imageName=$1
+	regex='(sys-img)-([^\"]*)-(android-?([^\"]*)-)([0-9][0-9])'
+	if [[ $imageName =~ $regex ]] ; then
+		local arch=${BASH_REMATCH[2]}
+		local device_type='default'		
+		if [ -n "${BASH_REMATCH[4]}" ]; then
+			# TV or Wear  		  
+			local device_type="android-${BASH_REMATCH[4]}"
+		fi		
+		local version=${BASH_REMATCH[5]}
+		#echo $imageName
+		local dir="$ANDROID_HOME/system-images/android-$version/$device_type/$arch"
+		if [ -d "$dir" ]; then
+			return 0
+		fi
+	else
+		echo 'Regex failed'
+	fi	
+	return 1
 }
 
 # Android SDK Build-tools
@@ -33,31 +66,44 @@ if [ "${platform_tools}" = 'both' ]; then
 	appendFilter 'platform-tools-preview'
 fi
 
-# Android SDK Platform-
+# Android SDK Platform
 
 IFS=',' read -ra ADDR <<< "$platform"
 for i in "${ADDR[@]}"; do
-    appendFilter "android-$i"
+	if checkIfIsInstalled "platforms" "android-$i" ; then
+	    echo "Android SDK Platform version $i is installed, skipping"
+	else
+		appendFilter "android-$i"
+	fi  	    
 done
 
 # Android SDK Build-tools versions
 
 IFS=',' read -ra ADDR <<< "$build_tools"
-for i in "${ADDR[@]}"; do
-    appendFilter "build-tools-$i"
+for i in "${ADDR[@]}"; do	
+	if checkIfIsInstalled "build-tools" "$i" ; then
+	    echo "Android SDK Build-tools version $i is installed, skipping"
+	else
+		appendFilter "build-tools-$i"
+	fi    
 done
 
 # SystemImage
 
 IFS=',' read -ra ADDR <<< "$system_images"
 for i in "${ADDR[@]}"; do
-    appendFilter "$i"
+	if checkIfIsSystemImageInstalled "$i" ; then
+	    echo "$i is already installed, skipping"
+	else
+		appendFilter "$i"
+	fi  	
 done
 
 echo "updating sdk using filter = $FILTER"
 
+
 # Prevent erros when accepting the license as suggest in:
 # http://stackoverflow.com/a/31900427/1107651
 ( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) \
-    | android update sdk --no-ui --all \
-    --filter ${FILTER}
+    | android update sdk --no-ui --all --filter ${FILTER}
+    
